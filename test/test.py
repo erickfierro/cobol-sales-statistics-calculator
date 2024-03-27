a9511331da26ebfa874322a9e51cf298519f474b
= <stevperdomo2017@gmail.com>
2024-03-27 18:43:17 -0500

    :sparkles: Upload structure and necessary files

---
+++ test/test.py
@@ -0,0 +1,12 @@
+# Importamos las librerias necesarias
+import unittest
+import subprocess
+
+class TestFunciones(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+# Ejecución de las pruebas
+if __name__ == '__main__':
+    unittest.main()
