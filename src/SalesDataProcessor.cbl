f8d4850935171ff3ec766f24092037762b7dc3f2
Erick Fierro <stevperdomo2017@gmail.com>
2024-04-18 18:50:30 -0500

    :construction: Add File reading

--- src/SalesDataProcessor.cbl
+++ src/SalesDataProcessor.cbl
@@ -14,31 +14,74 @@
            DECIMAL-POINT IS COMMA.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
+           SELECT SALES-DATA-FILE ASSIGN TO 'data.csv'
+           ORGANIZATION IS LINE SEQUENTIAL
+           FILE STATUS IS FS-STATUS.
       *----------------------------------------------------------------*
       *                   D A T A   D I V I S I O N                    *
       *----------------------------------------------------------------*
        DATA DIVISION.
        FILE SECTION.
+       FD SALES-DATA-FILE.
+       01 SALES-DATA-RECORD.
+           05 PRODUCT-NAME                 PIC X(60).
+           05 FILLER                       PIC X(01).
+           05 UNIT-PRICE                   PIC 9(08).
+           05 FILLER                       PIC X(01).
+           05 QUANTITY-SOLD                PIC 9(04).
+       01 END-OF-FILE                      PIC X(01) VALUE 'N'.
       *----------------------------------------------------------------*
       *          W O R K I N G  S T O R A G E   S E C T I O N          *
       *----------------------------------------------------------------*
        WORKING-STORAGE SECTION.
-      
+      *----------------------------------------------------------------*
+      *                           VARIABLES                            *
+      *----------------------------------------------------------------*
+       77 FS-STATUS                        PIC X(02).
+
        LINKAGE SECTION.
        01 DFHCOMMAREA                      PIC X(2534).
       
       *----------------------------------------------------------------*
-      *               P R O C E D U R E   D I V I S I O N               *
+      *               P R O C E D U R E   D I V I S I O N              *
       *----------------------------------------------------------------*
        PROCEDURE DIVISION.
            PERFORM 1000-INICIO-PROGRAMA
            PERFORM 2000-PROCESO-PROGRAMA
-           PERFORM 3000-FINAL-PROGRAMA.
+           PERFORM 3000-FINAL-PROGRAMA
+           .
       
       *----------------------------------------------------------------*
       * 1000-INICIO-PROGRAMA                                           *
       *----------------------------------------------------------------*
        1000-INICIO-PROGRAMA.
+           PERFORM 1100-ABRIR-ARCHIVO
+           PERFORM 1200-LEER-DATOS
+           .
+      
+      *----------------------------------------------------------------*
+      * 1100-ABRIR-ARCHIVO                                             *
+      *----------------------------------------------------------------*
+       1100-ABRIR-ARCHIVO.
+           OPEN INPUT SALES-DATA-FILE
+           IF FS-STATUS NOT EQUAL '00'
+               DISPLAY "ERROR OPENING FILE"
+               PERFORM 3000-FINAL-PROGRAMA
+           END-IF
+           PERFORM 1200-LEER-DATOS
+           CLOSE SALES-DATA-FILE
+           .
+
+      *----------------------------------------------------------------*
+      * 1200-LEER-DATOS                                                *
+      *----------------------------------------------------------------*
+       1200-LEER-DATOS.
+           PERFORM UNTIL END-OF-FILE = 'Y'
+               READ SALES-DATA-FILE INTO SALES-DATA-RECORD
+                   AT END 
+                       MOVE 'Y'        TO END-OF-FILE
+                END-READ
+           END-PERFORM
            .
       
       *----------------------------------------------------------------*
