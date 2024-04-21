48bfa9ab9f2f3999bc7c7469c062765589d39f01
Erick Fierro <stevperdomo2017@gmail.com>
2024-04-20 21:04:07 -0500

    :hammer: Search in the src/ folder for compile all the programs inside

--- compile.sh
+++ compile.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-compile_program() {
-    local program_name=$1
-    local source_file="src/$program_name.cbl"
-    local output_file="bin/$program_name"
+# Search in the src/ folder for compile all the programs inside
+find src/ -type f -name "*.cbl" -print0 | while IFS= read -r -d '' archivo; do
 
-    cobc -x -o "$output_file" "$source_file" >/dev/null 2>&1
+    nombre_archivo=${archivo##*/}
+    nombre_archivo=${nombre_archivo%.cbl}
+    
+    cobc -x -o "bin/$nombre_archivo" "$archivo" >/dev/null 2>&1
 
     if [ $? -ne 0 ]; then
-        echo "Error compiling the program $program_name"
+        echo "Error compiling the program $nombre_archivo"
         exit 1
     fi
-}
-
-compile_program SalesDataProcessor
-compile_program SalesStatisticsCalculator
-compile_program ResultsPrinter
-
-echo "Compilation completed"
+done
